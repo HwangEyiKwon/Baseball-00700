89303a2b16a40614dc7ce1627c6d2ec046dddbcd
HwangEyiKwon <hwang21035@naver.com>
2024-05-30 15:20:56 +0900

    확장성 있는 코드로 변경.

--- Project14/baseball.cpp
+++ Project14/baseball.cpp
@@ -10,47 +10,43 @@ struct GuessResult {
 
 class Baseball {
 public:
+    const int NUMBER_NUM = 3;
+
     explicit Baseball(const std::string& question)
         : question(question) {
-
     }
 
     GuessResult guess(const string& guessNumber) {
         assertIllegalArgument(guessNumber);
-        if (guessNumber == question) {
-            return { true, 3, 0 };
-        }
-        if (guessNumber != question) {
-            return { false, getStrikes(guessNumber), getBalls(guessNumber) };
-        }
-        return{ false, 0, 0 };
+        if (guessNumber == question) return { true, NUMBER_NUM, 0 };
+        return { false, getStrikes(guessNumber), getBalls(guessNumber) };
     }
 
     int getBalls(const std::string& guessNumber)
     {
         int balls = 0;
-        if (guessNumber[0] == question[1]) balls++;
-        if (guessNumber[0] == question[2]) balls++;
-        if (guessNumber[1] == question[0]) balls++;
-        if (guessNumber[1] == question[2]) balls++;
-        if (guessNumber[2] == question[0]) balls++;
-        if (guessNumber[2] == question[1]) balls++;
+        for (int i = 0; i < NUMBER_NUM; i++) {
+            for (int j = 0; j < NUMBER_NUM; j++) {
+                if (i == j) continue;
+                if (guessNumber[i] == question[j]) balls++;
+            }
+        }
         return balls;
     }
 
     int getStrikes(const std::string& guessNumber)
     {
         int strikes = 0;
-        if (guessNumber[0] == question[0]) strikes++;
-        if (guessNumber[1] == question[1]) strikes++;
-        if (guessNumber[2] == question[2]) strikes++;
+        for (int i = 0; i < NUMBER_NUM; i++) {
+            if (guessNumber[i] == question[i]) strikes++;
+        }
         return strikes;
     }
 
     void assertIllegalArgument(const std::string& guessNumber)
     {
 
-        if (guessNumber.length() != 3) throw length_error("Must be three letters");
+        if (guessNumber.length() != NUMBER_NUM) throw length_error("Must be three letters");
 
         for (char ch : guessNumber) {
             if (ch >= '0' && ch <= '9') continue;
@@ -64,9 +60,12 @@ public:
 
     bool isDuplicatedNumber(const std::string& guessNumber)
     {
-        return guessNumber[0] == guessNumber[1] ||
-            guessNumber[0] == guessNumber[2] ||
-            guessNumber[1] == guessNumber[2];
+        for (int i = 0; i < NUMBER_NUM; i++) {
+            for (int j = i + 1; j < NUMBER_NUM; j++) {
+                if (guessNumber[i] == guessNumber[j]) return true;
+            }
+        }
+        return false;
     }
 
 private:
