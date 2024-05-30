fa0863d11d4d9eead70f2d248548d880d4911fed
HwangEyiKwon <hwang21035@naver.com>
2024-05-30 14:32:53 +0900

    Step1_red&green

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,10 +1,11 @@
 #include "pch.h"
+#include "../Project14/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(A, B) {
+    EXPECT_EQ(1, 1);
+}
+
+TEST(BaseballGame, ThrowExxceptionWhenInputLengthIsUnmatched) {
+    Baseball game;
+    EXPECT_THROW(game.guess(string("12")), length_error);
 }
