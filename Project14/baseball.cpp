9a29718596a3ecba8d91c3532481981cfc43a465
HwangEyiKwon <hwang21035@naver.com>
2024-05-30 14:33:34 +0900

    Step1_refactor

--- Project14/baseball.cpp
+++ Project14/baseball.cpp
@@ -5,6 +5,6 @@ using namespace std;
 class Baseball {
 public:
     void guess(const string& str) {
-        throw length_error("Must be three letters");
+        if(str.length() != 3) throw length_error("Must be three letters");
    }
 };
